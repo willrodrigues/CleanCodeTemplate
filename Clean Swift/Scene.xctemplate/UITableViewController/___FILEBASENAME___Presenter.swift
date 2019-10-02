8c83bdc18cc6ea4d581871ca4eee355c76eeb2e2
Willian Rodrigues <willian.rodrigues.ext@panonline.com.br>
2019-10-02 14:13:29 -0300

    Adding initial version cloned from an existent project

---
+++ Clean Swift/Scene.xctemplate/UITableViewController/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,24 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+protocol ___VARIABLE_sceneName___PresentationLogic {
+    func presentSomething(response: ___VARIABLE_sceneName___.Model.Response)
+}
+
+class ___VARIABLE_sceneName___Presenter: ___VARIABLE_sceneName___PresentationLogic {
+    weak var viewController: ___VARIABLE_sceneName___DisplayLogic?
+    
+    // MARK: Do something
+    
+    func presentSomething(response: ___VARIABLE_sceneName___.Model.Response) {
+        let viewModel = ___VARIABLE_sceneName___.Model.ViewModel()
+        viewController?.displaySomething(viewModel: viewModel)
+    }
+}
